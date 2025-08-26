7f62643d0a60331057d803067a53e356d2735b6c
denisgrushkin <denis.grushkin@maddevs.io>
2025-08-26 11:01:36 +0600

    Add IBooking interface to define booking structure with relevant properties including id, title, description, shopId, artistId, startTime, endTime, date, and status.

---
+++ src/interfaces/booking.ts
@@ -0,0 +1,11 @@
+export interface IBooking {
+  id: number;
+  title: string;
+  description: string;
+  shopId: number;
+  artistId: number;
+  startTime: string;
+  endTime: string;
+  date: string;
+  status: string;
+}
