0cb18cb5e0d44e95ff88713d5b9e4bd5a2564794
denisgrushkin <denis.grushkin@maddevs.io>
2025-09-02 18:16:23 +0600

    Update package version to 0.0.7, implement useArtists module for artist data management with Supabase, and enhance IndexPage to dynamically load artists with improved loading state handling.

---
+++ src/stores/artists.ts
@@ -0,0 +1,16 @@
+import { defineStore } from "pinia";
+import type { IArtist } from "src/interfaces/artist";
+
+export const useArtistsStore = defineStore('artists', {
+  state: () => ({
+    artists: [] as IArtist[],
+  }),
+  getters: {
+    getArtists: (state) => state.artists,
+  },
+  actions: {
+    setArtists(artists: IArtist[]) {
+      this.artists = artists;
+    },
+  },
+});
