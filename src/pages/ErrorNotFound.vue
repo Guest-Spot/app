1dd4e1a29694257668765070edcd9128ec4f6405
denisgrushkin <denis.grushkin@maddevs.io>
2025-09-05 16:36:44 +0600

    Refactor ErrorNotFound component layout and styling. Update template structure to use q-page and enhance visual elements for better user experience. Modify button label and add icon for navigation.

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,23 +1,29 @@
 <template>
-  <div class="page fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
-    <div>
-      <div style="font-size: 30vh">404</div>
+  <q-page class="page q-py-md flex column items-center justify-center">
+    <div class="container">
+      <div class="flex column items-center q-gap-md text-center">
+        <div class="text-primary" style="font-size: 10rem; font-weight: 700; line-height: 1">404</div>
 
-      <div class="text-h2" style="opacity: 0.4">Oops. Nothing here...</div>
+        <div class="flex column items-center">
+          <h3 class="text-h6 q-mb-sm">Page not found</h3>
+          <p class="text-grey-6 text-center q-mb-xl q-px-xl">The requested page does not exist or has been moved</p>
+        </div>
 
-      <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
-        to="/"
-        label="Go Home"
-        no-caps
-      />
+        <q-btn
+          to="/"
+          label="Go to home"
+          icon="home"
+          class="bg-block q-px-lg back-btn"
+          rounded
+          unelevated
+        />
+      </div>
     </div>
-  </div>
+  </q-page>
 </template>
 
-<script setup lang="ts">
-//
-</script>
+<style scoped lang="scss">
+.back-btn {
+  min-height: 40px;
+}
+</style>
