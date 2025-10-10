712eed17fc1f675e43ee644e24a352fa8c0b8f56
denisgrushkin <denis.grushkin@maddevs.io>
2025-10-10 11:23:51 +0600

    Enhance ConnectProvider to set user profile after successful login
    - Integrated profile store to manage user profile data.
    - Updated login flow to store user profile upon successful authentication.
    - Improved user feedback with success notifications after login.

--- src/pages/connect/Provider.vue
+++ src/pages/connect/Provider.vue
@@ -31,11 +31,13 @@ import { useRoute, useRouter } from 'vue-router';
 import useUser from 'src/modules/useUser';
 import useNotify from 'src/modules/useNotify';
 import { AxiosError } from 'axios';
+import { useProfileStore } from 'src/stores/profile';
 
 const { showSuccess } = useNotify();
 const router = useRouter();
 const route = useRoute();
 const { fetchMe } = useUser();
+const profileStore = useProfileStore();
 
 defineOptions({
   name: 'ConnectProvider',
@@ -51,7 +53,10 @@ onBeforeMount(async () => {
     try {
       const url = `/api/auth/${route.params.provider as string}/callback?access_token=${String(access_token.value || '')}&provider=${route.params.provider as string}`;
       await connect(url);
-      await fetchMe();
+      const userData = await fetchMe();
+      if (userData) {
+        profileStore.setUserProfile(userData);
+      }
       showSuccess('Login successful');
       setTimeout(() => {
         void router.push('/profile');
