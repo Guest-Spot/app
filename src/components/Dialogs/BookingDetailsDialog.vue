048d810cbc6529b8d234fe9bbd6ec5639ef88f74
denisgrushkin <denis.grushkin@maddevs.io>
2025-10-17 13:50:12 +0600

    Update BookingDetailsDialog and CreateBookingDialog for enhanced validation and user experience
    - Increased the maximum character limit for rejection notes in BookingDetailsDialog from 100 to 250, along with updated validation rules.
    - Added computed properties in CreateBookingDialog to manage form submission state and ensure schedule completeness before submission.
    - Improved user feedback by validating the schedule step before allowing booking submission.

--- src/components/Dialogs/BookingDetailsDialog.vue
+++ src/components/Dialogs/BookingDetailsDialog.vue
@@ -322,17 +322,17 @@ const confirmReactionChange = (reaction: EReactions) => {
         outlined: true,
         counter: true,
         rounded: true,
-        maxlength: 100,
+        maxlength: 250,
         color: 'primary',
         required: true,
         isValid: (val: string) => {
           const trimmedValue = val.trim();
-          return trimmedValue.length > 0 && trimmedValue.length <= 100;
+          return trimmedValue.length > 0 && trimmedValue.length <= 250;
         },
         rules: [
           (val: string) => !!val.trim() || 'Reason is required',
           (val: string) =>
-            val.trim().length <= 100 || 'Reason must be less than 100 characters',
+            val.trim().length <= 250 || 'Reason must be less than 250 characters',
         ],
       },
       cancel: {
